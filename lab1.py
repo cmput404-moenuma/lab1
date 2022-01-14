8d79f50e67f005e0a9d6568858eaf7c31a72c2e7
moenuma <moenumasawa@gmail.com>
2022-01-14 14:32:14 -0700

    download itself

--- lab1.py
+++ lab1.py
@@ -1,6 +1,9 @@
 import requests
-response = requests.get("https://google.com/")
+# response = requests.get("https://google.com/")
 # print(requests.__version__)
-print (response.status_code)
-print (response.content)
+# print (response.status_code)
+# print (response.content)
 
+url = "https://raw.githubusercontent.com/moenuma/404lab1/master/lab1.py?token=GHSAT0AAAAAABQEZJ6ORTTEMC767TZLGHEWYPLEXEA"
+r = requests.get(url, allow_redirects=True)
+open('download.py', 'wb').write(r.content)
